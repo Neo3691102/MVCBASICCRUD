771712b140cc49be716a84c9fb74e1bb4afca661
Neo3691102 <nestor8772@gmail.com>
2024-01-22 21:22:28 -0600

    Agregar archivos de proyecto.

---
+++ MVCBASICCRUD/Models/CRUDMVCContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MVCBASICCRUD.Models
+{
+    public partial class CRUDMVCContext : DbContext
+    {
+        public CRUDMVCContext()
+        {
+        }
+
+        public CRUDMVCContext(DbContextOptions<CRUDMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+  //              optionsBuilder.UseSqlServer("server=NESTOR\\SQLEXPRESS01; database=CRUDMVC; integrated security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Clave)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("clave");
+
+                entity.Property(e => e.Fecha)
+                    .HasColumnType("date")
+                    .HasColumnName("fecha");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ MVCBASICCRUD/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCBASICCRUD.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string? Clave { get; set; }
+    }
+}
